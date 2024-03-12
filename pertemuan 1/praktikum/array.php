f033bb2801796967314b40e2d5d3fd8ccc0812da
audynairalya <146051657+audynairalya@users.noreply.github.com>
2024-03-12 12:41:32 +0700

    Add files via upload

---
+++ pertemuan 1/praktikum/array.php
@@ -0,0 +1,38 @@
+<?php
+$fruits = ["Mango", "Banana", "Strawberry", "Durian"];
+    // Default
+    // Associative Array
+    // Multidimensional Array
+
+        // var_dump ($fruits);
+        // echo '<br>';
+        // print_r ($fruits);
+        // echo '<br';
+        // foreach ($fruits as $fruit) {
+        //     echo '<br>';
+        //     echo '<br>';
+        //     echo $fruit;
+        //     echo '<br>';
+        // }
+
+        $dinos = [
+            [
+                'dinoName' => 'Rex',
+                'dinoAge' => 500
+            ],
+            [
+                'dinoName' => 'Bracio',
+                'dinoAge' => 800
+            ],
+            [
+                'dinoName' => 'Ptena',
+                'dinoAge' => 250
+            ]
+        ];
+
+        foreach ($dinos as $dino) {
+            echo $dino['dinoName'];
+            echo $dino['dinoAge'];
+            echo '<br>';
+        }
+?>
---
+++ pertemuan 1/praktikum/variable.php
@@ -0,0 +1,8 @@
+<?php
+    $dinoName = 'Rex';
+    $dinoAge = 300;
+
+    echo $dinoName;
+    echo '<br>';
+    echo $dinoAge;
+?>
